a48d8e6c1ed059200c00e11cb1c75e23e4f84499
Nausher Rao <SherRao15@gmail.com>
2021-08-22 19:01:01 -0400

    Initial file structure

---
+++ src/LandingPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+export default function LandingPage() {
+    return <p>hey</p>;
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import LandingPage from "./LandingPage";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+
+                <Route>
+                    <Redirect to="/" />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    </React.StrictMode >,
+    document.getElementById("root")
+);
