06b8d14a255a8a6668d20d71bb89011f022b4555
Robert336 <rmazza33@gmail.com>
2021-08-23 15:53:08 -0400

    Fix responsivness issues with Hero

--- src/MacWindow.jsx
+++ src/MacWindow.jsx
@@ -4,31 +4,18 @@ import window from "./Assets/Window.svg";
 
 
 const StyleContainer = Styled.div`
-    display: flex;
-    flex-direction: column;    
-
-    position: absolute;
     
     background: url(${window});
     background-repeat: no-repeat;
 
-    filter: drop-shadow(1px 10px 47px rgba(0, 0, 0, 0.8));
-
-    top: 25%;
-    left: 10%;
+    
     border-radius: 0.5em;
-    display: flex;
-    align-items: center;
-    z-index: 2;
-
-    min-width: 300px;
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
+    
   `;
 
-export default function MacWindow({ width, height, children }) {
+export default function MacWindow({ width, height, className, children }) {
     return (
-        <StyleContainer width={width} height={height}>
+        <StyleContainer width={width} height={height} className={className}>
             {children}
         </StyleContainer>
     );
