d45665f6aeefdce1f89dd4b03a698c0ddc8c48ae
Jagveer-Sangha <jagveersangha@gmail.com>
2021-08-27 20:56:33 -0400

    Add Socials

--- src/LandingPageFooter.jsx
+++ src/LandingPageFooter.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Styled from 'styled-components';
 import footer from "./Assets/Footer.svg";
-
+import tiktok from "./Assets/TikTok.png"
+import discord from "./Assets/Discord.png"
+import twitter from "./Assets/Twitter.png"
+import facebook from "./Assets/Facebook.png"
+import linkedin from "./Assets/LinkedIn.png"
+import youtube from "./Assets/YouTube.png"
+import instagram from "./Assets/Instagram.png"
 
 const StyleContainer = Styled.div`
     
@@ -12,16 +18,55 @@ const StyleContainer = Styled.div`
     
 
   `;
+const Socials = Styled.div`
+    text-align: center;
+    margin-top: auto;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: auto;
+    :hover {
+        cursor: pointer;
+        transform: translateY(-5px);
+    }`;
+
 
 
 
 export default function LandingPageFooter({ width, height, className, children }) {
     return (
-        <StyleContainer width={width} height={height} className={className}>
-            {children}
-        </StyleContainer>
+        <div>
+            <StyleContainer width={width} height={height} className={className}>
+                {children}
+            </StyleContainer>
+
+            <Socials width={width} height={height} className={className} >
 
 
+                <a href="#">
+                    <img src={discord} alt="Discord icon" />
+                </a>
+                <a href="#">
+                    <img src={instagram} alt="Instagram icon" />
+                </a>
+                <a href="#">
+                    <img src={twitter} alt="Twitter icon" />
+                </a>
+                <a href="#">
+                    <img src={facebook} alt="Facebook icon" />
+                </a>
+                <a href="#">
+                    <img src={linkedin} alt="LinkedIn icon" />
+                </a>
+                <a href="#">
+                    <img src={youtube} alt="Youtube icon" />
+                </a>
+                <a href="#">
+                    <img src={tiktok} alt="TikTok icon" />
+                </a>
+
+            </Socials>
+
+        </div>
     );
 }
 
