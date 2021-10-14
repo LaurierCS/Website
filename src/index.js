0ad464f4049ad55553c28c46f657bcadced9a80c
Robert336 <rmazza33@gmail.com>
2021-10-13 22:52:29 -0400

    create redirect to pods website at .../pods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect, Link } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import UpdateProfile from "./UpdateProfile";
 import Login from "./Login";
@@ -19,6 +19,10 @@ ReactDOM.render(
                     <Route exact path="/" component={LandingPage} />
                     <Route path="/login" component={Login} />
                     <Route path="/forgot-password" component={ForgotPassword} />
+                    <Route path="/pods" component={() => {
+                        window.location.href = 'https://admin478456.wixsite.com/pods';
+                        return null;
+                    }} />
                     <PrivateRoute path="/dashboard" component={Dashboard} />
                     <PrivateRoute path="/update-profile" component={UpdateProfile} />
                     <Route>
