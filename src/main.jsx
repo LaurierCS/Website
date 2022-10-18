a33fc580da6ce7d25235cb395e3ccbd487a4237f
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-18 18:36:42 -0400

    Add root

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './main.css';
+import Router from './utils/Router';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router/>
+  </React.StrictMode>
+)
