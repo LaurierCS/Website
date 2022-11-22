2aaedee7d9c86317dc761c1ace9ffb556b0a0d60
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-22 05:51:11 -0500

    Create navbar component files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+     return (
+          <div className="navbar">
+               
+          </div>
+     );
+};
+
+export default Navbar;
