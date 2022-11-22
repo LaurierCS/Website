42eeb3639e72a0b7892e3d3f7bd6d444659bde18
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-22 05:50:25 -0500

    Update landing page and styling

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css';
+import { Navbar } from '../../components';
+
+const Landing = () => {
+     return (
+          <>
+               <Navbar />
+          </>
+     );
+};
+
+export default Landing;
