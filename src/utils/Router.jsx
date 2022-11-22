d1cd5244fb43cc5b99a9bdfc54cd7282b0604e0b
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-22 05:50:51 -0500

    Update router utility

--- src/utils/Router.jsx
+++ src/utils/Router.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing } from '../pages';
 
 const Router = () => {
      return (
           <BrowserRouter>
                <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Landing />} />
                </Routes>
           </BrowserRouter>
      )
