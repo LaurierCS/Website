749c45eeb5c1af1731ffc8b1072f94b8497e8cdc
Ali Akbar <91800364+Ali-A132@users.noreply.github.com>
2022-12-07 17:22:34 -0500

    Create accordion component and add styling

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './accordion.css';
+import { Illustration } from '../../assets';
+
+const Accordion = () => {
+     return (
+          <div className="accordion">
+               <Illustration />
+               
+          </div>
+     );
+};
+
+export default Accordion;
