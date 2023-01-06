743b8ad3827ce88d4932554a5158882199adefda
Ali Akbar <91800364+Ali-A132@users.noreply.github.com>
2023-01-05 21:24:11 -0500

    Add accordion menu to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Landing.css';
 import { Navbar } from '../../components';
+import Accord from '../../components/Accord/Accord';
 
 const Landing = () => {
      return (
           <>
                <Navbar />
+               <Accord />
           </>
      );
 };
