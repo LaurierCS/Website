c01c48b1f8963cc2bcc53e8d68e69551d447ed65
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-01-20 18:55:14 -0500

    Update exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,4 @@
 import Navbar from './Navbar/Navbar';
-<<<<<<< HEAD
 import Card from './Card/Card';
 import Footer from './Footer/Footer';
 import Socials from './Socials/Socials';
@@ -10,15 +9,3 @@ export {
     Footer,
     Socials,
 }
-=======
-import Footer from './Footer/Footer';
-
-export {
-     Navbar,
-}
-
-
-export {
-     Footer
-}
->>>>>>> aa68f189a86f7c6231cd1f94aa73fd3f08aa7321
