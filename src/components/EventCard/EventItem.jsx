13df6bcef789895116256fbce67fd91bc1d19f80
noy00y <69055108+noy00y@users.noreply.github.com>
2023-01-25 03:10:08 -0500

    adding eventItem component

---
+++ src/components/EventCard/EventItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './EventCard.css';
+import EventTag from './EventTag';
+
+// Item Component:
+/*
+    Component Details:
+    - handles state updates for event and event tags
+*/
+
+class EventItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: [] // array of tags to be updated with state
+        }
+    }
+
+    // Updates Tag array
+    addTag(tag) {
+        let currentTags = this.state.tags;
+        currentTags.push(tag);
+        this.setState({tags: currentTags});
+    }
+
+    render() {
+        return (
+            <div className = "event_item">
+                <h1>{props.title}</h1>
+                {this.state.tags.map((tag, key) => {
+                    return <div key = {key}>{tag}</div> // creates a tag component
+                })}
+            </div>
+        );
+    }
+}
+
+export default EventItem;
