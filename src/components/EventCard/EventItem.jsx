a13d6cfe2eef4b898f807a1cd2c0a574bc4f4093
juancwu <46619361+juancwu@users.noreply.github.com>
2023-01-26 11:03:28 -0500

    update: css import name

--- src/components/EventCard/EventItem.jsx
+++ src/components/EventCard/EventItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './EventCard.css';
-import EventTag from './EventTag';
+import './EventItem.css';
+import EventTag from '../EventTag/EventTag';
 
 // Item Component:
 /*
@@ -12,23 +12,27 @@ class EventItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tags: [] // array of tags to be updated with state
-        }
+            tags: [], // array of tags to be updated with state
+        };
     }
 
     // Updates Tag array
     addTag(tag) {
         let currentTags = this.state.tags;
         currentTags.push(tag);
-        this.setState({tags: currentTags});
+        this.setState({ tags: currentTags });
     }
 
     render() {
         return (
-            <div className = "event_item">
+            <div className="event_item">
                 <h1>{props.title}</h1>
                 {this.state.tags.map((tag, key) => {
-                    return <div className = "item_tags" key = {key}>{tag}</div> // creates a tag component
+                    return (
+                        <div className="item_tags" key={key}>
+                            {tag}
+                        </div>
+                    ); // creates a tag component
                 })}
             </div>
         );
