39c4d32e80a1d9b22d3e09ac0eb99ea80756a7bc
juancwu <46619361+juancwu@users.noreply.github.com>
2023-01-27 17:17:08 -0500

    update: responsive design for InfoBanner

--- src/components/InfoBanner/InfoBanner.jsx
+++ src/components/InfoBanner/InfoBanner.jsx
@@ -5,29 +5,35 @@ import { Center } from '@mantine/core';
 import CountUp from 'react-countup';
 
 const InfoBanner = () => {
-     return (
-          <section id="info-banner">    
-               <InfoDividerTop />
-               <Center style={{backgroundColor: "#12181B"}}>
-                    <div className="info__banner">
-                         <div className="info__banner__text">
-                              <p className="info__header"> Community of </p>
-                              <p className="info__stats">{<CountUp end={3000} duration={1.0} />}+</p>
-                              <p className="info__header"> CS Students </p>
-                         </div>
-                         <div className="info__banner__text">
-                              <p className="info__stats">{<CountUp end={20} duration={1.0} />}+</p>
-                              <p className="info__header"> Events in the Year </p>
-                         </div>
-                         <div className="info__banner__text">
-                              <p className="info__stats">{<CountUp end={52} duration={1.0} />}</p>
-                              <p className="info__header"> Executive Members </p>
-                         </div>
+    return (
+        <section id="info-banner">
+            <InfoDividerTop />
+            <Center style={{ backgroundColor: '#12181B' }}>
+                <div className="info__banner">
+                    <div className="info__banner__text">
+                        <p className="info__header"> Community of </p>
+                        <p className="info__stats">
+                            {<CountUp end={3000} duration={1.0} />}+
+                        </p>
+                        <p className="info__header"> CS Students </p>
                     </div>
-               </Center>
-               <InfoDividerBottom />
-          </section>
-     );
+                    <div className="info__banner__text">
+                        <p className="info__stats">
+                            {<CountUp end={20} duration={1.0} />}+
+                        </p>
+                        <p className="info__header"> Events in the Year </p>
+                    </div>
+                    <div className="info__banner__text">
+                        <p className="info__stats">
+                            {<CountUp end={52} duration={1.0} />}
+                        </p>
+                        <p className="info__header"> Executive Members </p>
+                    </div>
+                </div>
+            </Center>
+            <InfoDividerBottom />
+        </section>
+    );
 };
 
 export default InfoBanner;
