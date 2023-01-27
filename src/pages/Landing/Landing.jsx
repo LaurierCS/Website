d193d45ba69711af14da9cc6bfac3f530404def0
Mobina Toorani <mobinatoorani@gmail.com>
2023-01-27 04:49:09 -0500

    Add: About

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Landing.css';
-import { Navbar, Footer, EventSection } from '@components';
+import { Navbar, Footer, About, EventSection } from '@components';
 
 const Landing = () => {
     return (
         <>
             <Navbar />
+            <About />
             <EventSection />
             <Footer />
         </>
