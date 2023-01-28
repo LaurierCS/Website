b8e3578afec3cf0104d945ff6ecfe0dd3fbf4708
sshakibbb <shakibjadside01@gmail.com>
2023-01-28 15:12:57 -0500

    Initial commit to setup branch

---
+++ src/components/Newsletter/newsletter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Newsletter.css"
+import { Title, Text, Container, Grid, Flex, Center} from '@mantine/core';
+
+const NEWSLETTER_HEADING = "<Join our Newsletter!/>";
+const NEWSLETTER_CAPTION = "<Subscribe to our newsletter to receive event<br>announcements and more!/>"
+
+const Newsletter = () => {
+    return
+}
