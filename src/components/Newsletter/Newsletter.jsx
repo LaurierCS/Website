68dd187c167075a3755920d4c36fae7dd5119961
sshakibbb <shakibjadside01@gmail.com>
2023-01-29 16:35:11 -0500

    Fix: Component Styling

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -3,13 +3,12 @@ import "./Newsletter.css"
 import { Title, Text, Container, Flex, Center } from '@mantine/core';
 
 const NEWSLETTER_HEADING = "Join our Newsletter!";
-const NEWSLETTER_CAPTION = "Subscribe to our newsletter to receive event \
-announcements and more!"
+const NEWSLETTER_CAPTION = ["Subscribe to our newsletter to receive event", <br/>, "announcements and more!"]
 
 const Newsletter = () => {
     return (
         <section id="newsletter">
-            <Container size="x1" my="md">
+            <Container size="x1" my={300}>
                 <Center style={{ width: '100%', height: '100%'}}>
                     <Flex
                         direction="column"
@@ -17,10 +16,10 @@ const Newsletter = () => {
                         wrap="wrap"
                         gap="md"
                     >
-                        <Title align="center" variant="gradient" order={1}>
+                        <Title align="center" variant="gradient" order={1} size={50}>
                             {NEWSLETTER_HEADING}
                         </Title>
-                        <Text align="center" color="white">
+                        <Text align="center" color="white" size={30}>
                             {NEWSLETTER_CAPTION}
                         </Text>
                         
