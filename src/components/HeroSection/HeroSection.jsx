a6b8f2a66f0d9843844526617cdff0924daa982e
Jacqueline Truong <itsjacquellinetruong@gmail.com>
2023-01-30 06:17:24 -0500

    update: hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,6 +1,6 @@
 import './HeroSection.css';
 import { Title, Text, BackgroundImage, Container, Flex, Image, Center, createStyles, Button } from "@mantine/core";
-import { CorpImage, IconLogo, DiscordLogo } from '@assets';
+import { CorpImage, IconLogo } from '@assets';
 import {FaDiscord} from "react-icons/fa";
 
 const HERO_SECTION_TITLE = '<Laurier Computing Society/>';
@@ -10,41 +10,51 @@ const HERO_SECTION_TEXT = "Some other mini text that talks about us lol";
 const HeroSection = () => {
     return (
         <section id="top">
-            <Container size="xl" my="md">
+            <Container py='8rem' size="xl" my="md">
                 <Center style={{ width: '100%', height: '100%' }}>
                     <Flex
-                    direction="column"
-                    justify="center"
-                    wrap="wrap"
-                    gap="md"
+                        direction="column"
+                        justify="center"
+                        wrap="wrap"
+                        gap="md"
                     >
                         {/*Leaving as a possible placeholder for bg img*/}
                         <BackgroundImage
                             src=""
                         > 
-                            {/*Not sure why the logo isn't being centered*/}
-                            <Image
-                                src={IconLogo}
-                                width={100}
-                                height={70}
-                                align="center"
-                                class="logo"
+                            {/*Icon Logo*/}
+                            <Center>
+                                <Image
+                                    src={IconLogo}
+                                    width={120}
+                                    height={90}
+                                    align="center"
+                                    pb="4rem"
                             />
-                            <Title align="center" size={40} fw={700} variant="gradient" order={1}>
+                            </Center>
+
+                            {/*Hero Text*/}
+                            <Title pb='10rem' align="center" size={55} fw={700} variant="gradient" order={1}>
                                 {HERO_SECTION_TITLE}
                             </Title>
                             <Text align="center" fz="xl" fw={500} c="white">
                                 {HERO_SECTION_HOOK}
                             </Text>
-                            <Text align="center" fz="xl" fw={300} c="white">
+                            <Text pt="2rem" align="center" fz="xl" fw={300} c="white">
                                 {HERO_SECTION_TEXT}
                             </Text>
-                            <Button
-                                variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}
-                                leftIcon={<FaDiscord/>}
-                            >
-                                Join our Discord
-                            </Button>
+
+                            {/*Discord Button*/}
+                            <Center py="5rem">
+                                <Button
+                                    variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }} size="lg"
+                                    leftIcon={<FaDiscord/>}
+                                >
+                                    Join our Discord!
+                                </Button>
+                            </Center>
+
+                            {/*Hero Image*/}
                             <Image
                                 src={CorpImage}
                                 height={750}
