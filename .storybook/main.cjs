4a1fa8c386ef1991ad852672a4b0b3955e9219d9
juancwu <46619361+juancwu@users.noreply.github.com>
2023-01-30 18:49:06 -0500

    update: hard-coded alias and fixed typo

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,4 +1,5 @@
 const { mergeConfig } = require('vite');
+const path = require('path');
 
 module.exports = {
     stories: [
@@ -17,9 +18,27 @@ module.exports = {
     features: {
         storyStoreV7: true,
     },
-    async viteFina(config) {
-        return mergeConfig(config, {
-            resolve: (await import('../vite.config.js')).default.resolve,
-        });
+    async viteFinal(config) {
+        // TODO: find a way to import the configurations
+        //       from `vite.config.js` instead of hard coding them
+        const customConfig = {
+            resolve: {
+                alias: {
+                    '@': path.resolve(__dirname, '../src/'),
+                    '@assets': path.resolve(__dirname, '../src/assets/'),
+                    '@components': path.resolve(
+                        __dirname,
+                        '../src/components/'
+                    ),
+                    '@pages': path.resolve(__dirname, '../src/pages/'),
+                    '@stories': path.resolve(__dirname, '../src/stories/'),
+                    '@utils': path.resolve(__dirname, '../src/utils/'),
+                },
+            },
+        };
+
+        const newConfig = mergeConfig(config, customConfig);
+
+        return newConfig;
     },
 };
