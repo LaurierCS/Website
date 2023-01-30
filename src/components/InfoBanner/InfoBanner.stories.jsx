9f6cbf9632e4600cb66da0f54868e196e0ece4e7
juancwu <46619361+juancwu@users.noreply.github.com>
2023-01-30 18:49:29 -0500

    add: example of a story with alias setup

---
+++ src/components/InfoBanner/InfoBanner.stories.jsx
@@ -0,0 +1,10 @@
+import { InfoBanner } from '@components';
+
+export default {
+    title: 'LCS Website 2023/Components/InfoBanner',
+    component: InfoBanner,
+};
+
+export const Default = () => {
+    return <InfoBanner />;
+};
