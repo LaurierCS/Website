9220175ef4c97beeb07937dff25dee03cff4a15c
juancwu <46619361+juancwu@users.noreply.github.com>
2023-02-01 21:10:37 -0500

    initial setup for firestore

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import "./scripts/config";
 import ReactDOM from 'react-dom/client';
 import './main.css';
 import Router from '@utils/Router';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/scripts/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.LCS_FIREBASE_API_KEY,
+  authDomain: import.meta.env.LCS_FIREBASE_AUTHDOMAIN,
+  databaseURL: import.meta.env.LCS_FIREBASE_DBURL,
+  projectId: import.meta.env.LCS_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.LCS_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.LCS_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.LCS_FIREBASE_APPID,
+  measurementId: import.meta.env.LCS_FIREBASE_MEASUREMENTID
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
