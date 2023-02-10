8c02d2436805552d07a1089f8cfe90925a13d41d
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-02-10 06:26:18 -0500

    Update hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,11 +1,13 @@
 import './HeroSection.css';
-import { Title, Text, BackgroundImage, Container, Flex, Image, Center, createStyles, Button } from "@mantine/core";
+import { Title, Text, BackgroundImage, Container, Flex, Image, Center, MediaQuery, Button } from "@mantine/core";
 import { CorpImage, IconLogo } from '@assets';
 import {FaDiscord} from "react-icons/fa";
 
-const HERO_SECTION_TITLE = '<Laurier Computing Society/>';
-const HERO_SECTION_HOOK = "Wilfrid Laurier University's official student operated computer science society.";
-const HERO_SECTION_TEXT = "Some other mini text that talks about us lol";
+const HERO_SECTION_TITLE = '<Laurier Computing Society />';
+const HERO_SECTION_HOOK = "The official Computer Science club at Wilfrid Laurier University!";
+const HERO_SECTION_TEXT_1 = "Student Lead";
+const HERO_SECTION_TEXT_2 = "Established 2012";
+const HERO_SECTION_TEXT_3 = "Career Driven";
 
 const HeroSection = () => {
     return (
@@ -23,26 +25,48 @@ const HeroSection = () => {
                             src=""
                         > 
                             {/*Icon Logo*/}
-                            <Center>
-                                <Image
-                                    src={IconLogo}
-                                    width={120}
-                                    height={90}
-                                    align="center"
-                                    pb="4rem"
-                            />
-                            </Center>
+                            <MediaQuery 
+                                query="(max-width: 768px)"
+                                styles={{ height: '0.5rem', marginBottom: '2rem' }}
+                            >
+                                <Center>
+                                    <Image
+                                        src={IconLogo}
+                                        width={120}
+                                        height={90}
+                                        align="center"
+                                        pb="4rem"
+                                />
+                                </Center>
+                            </MediaQuery>
 
                             {/*Hero Text*/}
-                            <Title pb='3rem' align="center" size={55} fw={700} variant="gradient" order={1}>
-                                {HERO_SECTION_TITLE}
-                            </Title>
-                            <Text align="center" fz="xl" fw={500} c="white">
-                                {HERO_SECTION_HOOK}
-                            </Text>
-                            <Text pt="2rem" align="center" fz="xl" fw={300} c="white">
-                                {HERO_SECTION_TEXT}
-                            </Text>
+                            <MediaQuery 
+                                query="(min-width: 768px)"
+                                styles={{ fontSize: '3rem' }}
+                            >
+                                <Title pb='3rem' align="center" fw={700} variant="gradient" order={1}>
+                                    {HERO_SECTION_TITLE}
+                                </Title>
+                            </MediaQuery>
+
+                            <MediaQuery 
+                                query="(max-width: 768px)"
+                                styles={{ fontSize: '1.2rem' }}
+                            >
+                                <Text align="center" fz="xl" fw={500} c="white">
+                                    {HERO_SECTION_HOOK}
+                                </Text>
+                            </MediaQuery>
+
+                            <MediaQuery 
+                                query="(max-width: 768px)"
+                                styles={{ fontSize: '1rem' }}
+                            >
+                                <Text pt="2rem" align="center" fz="xl" fw={300} c="white">
+                                    {HERO_SECTION_TEXT_1}&nbsp;&nbsp;|&nbsp;&nbsp;{HERO_SECTION_TEXT_2}&nbsp;&nbsp;|&nbsp;&nbsp;{HERO_SECTION_TEXT_3}
+                                </Text>
+                            </MediaQuery> 
 
                             {/*Discord Button*/}
                             <Center py="5rem">
