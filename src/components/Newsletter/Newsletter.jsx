579d961ebdf432d8edbeeaec568439c4a6306834
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-02-10 06:26:52 -0500

    Update newsletter

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Newsletter.css"
-import { Title, Text, Container, Flex, Center, TextInput, Button, Group, Box } from '@mantine/core';
+import { Title, Text, Container, Flex, Center, TextInput, Button, Group, Box, MediaQuery } from '@mantine/core';
 import { useForm } from '@mantine/form';
 
 const NEWSLETTER_HEADING = "Join our Newsletter!";
@@ -30,24 +30,33 @@ function Newsletter () {
                         <Title align="center" color="#A1DAF5" order={1}>
                             {NEWSLETTER_HEADING}
                         </Title>
-                        <Text align="center" color="white">
-                            {NEWSLETTER_CAPTION}
-                        </Text>
+                        <MediaQuery 
+                            query="(max-width: 768px)"
+                            styles={{ fontSize: '1.2rem' }}
+                        >
+                            <Text align="center" color="white">
+                                {NEWSLETTER_CAPTION}
+                            </Text>
+                        </MediaQuery>
 
-                        <Box sx={{ width: 400 }} mx="auto" mt="sm">
-                            <form onSubmit={form.onSubmit((values) => console.log(values))}>
-                                <TextInput
-                                withAsterisk
-                                placeholder="Email"
-                                {...form.getInputProps('email')}
-                                />
+                        <MediaQuery 
+                            query="(max-width: 768px)"
+                            styles={{ width: 300 }}
+                        >
+                            <Box sx={{ width: 400 }} mx="auto" mt="sm">
+                                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                                    <TextInput
+                                    withAsterisk
+                                    placeholder="Email"
+                                    {...form.getInputProps('email')}
+                                    />
 
-                                <Group position="center" mt="sm">
-                                    <Button fullWidth variant="filled" type="submit" size="lg" radius="md">SUBSCRIBE</Button>
-                                </Group>
-                            </form>
-                        </Box>
-                        
+                                    <Group position="center" mt="sm">
+                                        <Button fullWidth variant="filled" type="submit" size="lg" radius="md">SUBSCRIBE</Button>
+                                    </Group>
+                                </form>
+                            </Box>
+                        </MediaQuery>
                     </Flex>
                 </Center>
             </Container>
