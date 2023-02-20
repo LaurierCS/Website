12adcf1c0262a0ff53c727d7210c588e9e0b5372
juancwu <46619361+juancwu@users.noreply.github.com>
2023-02-20 03:50:37 -0500

    add firebase auth hook for easy access
    - no sign in / sign out functions

--- src/scripts/hooks/auth.jsx
+++ src/scripts/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
 import { HashLoader } from 'react-spinners';
 
@@ -11,16 +11,29 @@ const override = {
 
 export const AuthContext = React.createContext();
 
+export const useFirebaseAuth = () => {
+    const values = useContext(AuthContext);
+
+    if (!values) {
+        console.warn(
+            '`useFirebaseAuth` being used before successful authentication.'
+        );
+    }
+
+    return values;
+};
+
 const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState();
     const [pending, setPending] = useState(true);
     useEffect(() => {
-        getAuth().onAuthStateChanged((user) => {
+        const unsub = getAuth().onAuthStateChanged((user) => {
             if (user) {
                 setCurrentUser(user);
             }
             setPending(false);
         });
+        return unsub;
     }, []);
     if (pending) {
         return (
