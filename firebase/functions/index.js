1ce7799061d35e095cbdc3e0e90b35e3a86e55a3
juancwu <46619361+juancwu@users.noreply.github.com>
2023-03-06 00:21:43 -0500

    add callable function to grant admin permission

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,1 +1,10 @@
 import functions from 'firebase-functions';
+import admin from 'firebase-admin';
+
+import { handleGrantAdminPerms } from './callables/permissions.js';
+
+admin.initializeApp();
+
+export const grantAdminPermission = functions.https.onCall(
+    handleGrantAdminPerms
+);
