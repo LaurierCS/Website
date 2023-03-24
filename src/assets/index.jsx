6d37eccce7a02fbc1edcd9fce97121fe78ffe025
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-03-24 18:19:05 -0400

    Update asset exports

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,10 +2,12 @@ import Logo from './lcs_logo.png';
 import IconLogo from './logo_icon.svg';
 import Banner from './lcs_banner.png';
 import CorpImage from './corporate_artstyle.png';
+import PCDesign from './pc_design.svg';
 
 export {
      Logo,
      IconLogo,
      Banner,
      CorpImage,
+     PCDesign,
 }
