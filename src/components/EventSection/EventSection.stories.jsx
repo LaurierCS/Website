23fa622b0a88334617d3da2a269563e862576806
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-07-26 15:18:39 -0400

    Revert "Events section"

--- src/components/EventSection/EventSection.stories.jsx
+++ src/components/EventSection/EventSection.stories.jsx
@@ -1,11 +1,30 @@
-import EventSections from './EventSection'
+import EventSection from './EventSection';
 
 export default {
-  title: 'Components/EventSection',
-  component: EventSections,
-  tags: ['autodocs'],
-}
+    title: 'LCS Website 2023/Components/EventSection',
+    component: EventSection,
+};
 
-export const Default = {
-  args: {}
-}
+export const Desktop = () => {
+    return <EventSection />;
+};
+
+export const Mobile = () => {
+    return <EventSection />;
+};
+
+Mobile.parameters = {
+    viewport: {
+        defaultViewport: 'mobile2',
+    },
+};
+
+export const SmallFactorPhone = () => {
+    return <EventSection />;
+};
+
+SmallFactorPhone.parameters = {
+    viewport: {
+        defaultViewport: 'mobile1',
+    },
+};
--- src/utils/day.js
+++
@@ -1,6 +0,0 @@
-import dayjs from 'dayjs';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
-
-dayjs.extend(advancedFormat);
-
-export default dayjs;
