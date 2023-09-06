6f3c51f15b5272cfe5fe8178de65db16fca872be
jc <46619361+juancwu@users.noreply.github.com>
2023-09-05 20:00:32 -0400

    ui updates for newsletter section

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,59 +1,108 @@
-import React from "react";
-import "./Newsletter.css"
-import { Title, Text, Container, Flex, Center, TextInput, Button, Group, Box, MediaQuery } from '@mantine/core';
+import React from 'react';
+import {
+    Title,
+    Text,
+    Container,
+    Flex,
+    Center,
+    TextInput,
+    Button,
+    Group,
+    Box,
+    MediaQuery,
+    createStyles,
+} from '@mantine/core';
 import { useForm } from '@mantine/form';
 
-const NEWSLETTER_HEADING = "Join our Newsletter!";
-const NEWSLETTER_CAPTION = ["Subscribe to our newsletter to receive event", <br/>, "announcements and more!"]
+const NEWSLETTER_HEADING = '<Join our Newsletter! />';
+const NEWSLETTER_CAPTION = [
+    'Subscribe to our newsletter to receive event',
+    <br />,
+    'announcements and more!',
+];
 
-function Newsletter () {
+const useStyles = createStyles((theme) => ({
+    emailInput: {
+        backgroundColor: '#E7EBF5',
+        border: 'none',
+        color: '#1A1B1E',
+        textAlign: 'center',
+        borderRadius: '6px',
+        padding: '6px 16px',
+        width: '100%',
+    },
+}));
+
+function Newsletter() {
+    const { classes } = useStyles();
     const form = useForm({
         initialValues: {
             email: '',
         },
-
-        validate: {
-            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-        },
     });
+
+    const subscribe = async ({ email }) => {
+        if (!email) return;
+        console.log(email);
+    };
 
     return (
         <section id="newsletter">
-            <Container size="x1" my={300}>
-                <Center style={{ width: '100%', height: '100%'}}>
+            <Container size="x1" my={150}>
+                <Center style={{ width: '100%', height: '100%' }}>
                     <Flex
                         direction="column"
                         justify="center"
                         wrap="wrap"
                         gap="md"
                     >
-                        <Title align="center" color="#A1DAF5" order={1}>
+                        <Text align="center" color="white">
+                            Wanna stay in the know of our next events?"
+                        </Text>
+                        <Title
+                            align="center"
+                            variant="gradient"
+                            order={1}
+                            sx={{ fontSize: '3rem' }}
+                        >
                             {NEWSLETTER_HEADING}
                         </Title>
-                        <MediaQuery 
+                        <MediaQuery
                             query="(max-width: 768px)"
                             styles={{ fontSize: '1.2rem' }}
                         >
-                            <Text align="center" color="white">
-                                {NEWSLETTER_CAPTION}
+                            <Text align="center" color="white" weight="bold">
+                                Subscribe to our newsletter to receive event
+                                <br />
+                                announcements, updates, and more!
                             </Text>
                         </MediaQuery>
 
-                        <MediaQuery 
+                        <MediaQuery
                             query="(max-width: 768px)"
                             styles={{ width: 300 }}
                         >
                             <Box sx={{ width: 400 }} mx="auto" mt="sm">
-                                <form onSubmit={form.onSubmit((values) => console.log(values))}>
-                                    <TextInput
-                                    withAsterisk
-                                    placeholder="Email"
-                                    {...form.getInputProps('email')}
+                                <form onSubmit={form.onSubmit(subscribe)}>
+                                    <input
+                                        className={classes.emailInput}
+                                        placeholder="Email"
+                                        type="email"
+                                        required
+                                        {...form.getInputProps('email')}
                                     />
-
-                                    <Group position="center" mt="sm">
-                                        <Button fullWidth variant="filled" type="submit" size="lg" radius="md">SUBSCRIBE</Button>
-                                    </Group>
+                                    <Button
+                                        fullWidth
+                                        variant="filled"
+                                        type="submit"
+                                        radius="6px"
+                                        sx={{
+                                            fontSize: '1rem',
+                                            marginTop: '10px',
+                                        }}
+                                    >
+                                        SUBSCRIBE
+                                    </Button>
                                 </form>
                             </Box>
                         </MediaQuery>
