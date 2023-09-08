a32a098e817b843a28bb4a0fab42221c2a82d3c9
Nausher Rao <SherRao15@gmail.com>
2023-09-08 19:06:17 -0400

    Update metrics

--- src/components/InfoBanner/InfoBanner.jsx
+++ src/components/InfoBanner/InfoBanner.jsx
@@ -4,6 +4,10 @@ import { InfoDividerTop, InfoDividerBottom } from '@components';
 import { Center } from '@mantine/core';
 import CountUp from 'react-countup';
 
+const STUDENT_COUNT = 3500;
+const EVENT_COUNT = 20;
+const EXECUTIVE_COUNT = 54;
+
 const InfoBanner = () => {
     return (
         <section id="info-banner" style={{ lineHeight: 0 }}>
@@ -18,19 +22,19 @@ const InfoBanner = () => {
                 <div className="info__banner">
                     <div className="info__banner__text">
                         <p className="info__stats">
-                            {<CountUp end={3000} duration={1.0} />}+
+                            {<CountUp end={STUDENT_COUNT} duration={1.0} />}+
                         </p>
                         <p className="info__header"> Students </p>
                     </div>
                     <div className="info__banner__text">
                         <p className="info__stats">
-                            {<CountUp end={20} duration={1.0} />}+
+                            {<CountUp end={EVENT_COUNT} duration={1.0} />}+
                         </p>
                         <p className="info__header"> Events in the Year </p>
                     </div>
                     <div className="info__banner__text">
                         <p className="info__stats">
-                            {<CountUp end={54} duration={1.0} />}
+                            {<CountUp end={EXECUTIVE_COUNT} duration={1.0} />}
                         </p>
                         <p className="info__header"> Executive Members </p>
                     </div>
