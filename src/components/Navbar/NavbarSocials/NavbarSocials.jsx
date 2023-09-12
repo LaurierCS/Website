f67aea98f0310de8d5157c65eb8e8088344a7e0a
Steven Tohme <steventohme59@gmail.com>
2023-09-11 22:32:11 -0400

    add transition and social colors to social buttons

--- src/components/Navbar/NavbarSocials/NavbarSocials.jsx
+++ src/components/Navbar/NavbarSocials/NavbarSocials.jsx
@@ -6,11 +6,41 @@ import { FaYoutube, FaDiscord, FaTwitch } from 'react-icons/fa';
 
 const useStyles = createStyles(() => {
     return {
-        socials: {
+        discord: {
+            transition: 'all 200ms ease-in-out',
             '&:hover': {
-                transform: 'scale(1.1)'
+                transform: 'scale(1.2)',
+                color: '#7289da'
             }
         },
+        instagram: {
+            transition: 'all 200ms ease-in-out',
+            '&:hover': {
+                transform: 'scale(1.2)',
+                color: '#d62976'
+            }
+        },
+        linkedin: {
+            transition: 'all 200ms ease-in-out',
+            '&:hover': {
+                transform: 'scale(1.2)',
+                color: '#0a66c2'
+            }
+        },
+        youtube: {
+            transition: 'all 200ms ease-in-out',
+            '&:hover': {
+                transform: 'scale(1.2)',
+                color: '#c4302b'
+            }
+        },
+        twitch: {
+            transition: 'all 200ms ease-in-out',
+            '&:hover': {
+                transform: 'scale(1.2)',
+                color: '#6441a5'
+            }
+        }
     };
 });
 
@@ -24,7 +54,7 @@ const NavbarSocials = () => {
                     target="_blank"
                     rel="external noreferrer"
                 >
-                    <ActionIcon size="md" variant="transparent" className={classes.socials}>
+                    <ActionIcon size="md" variant="transparent" className={classes.discord}>
                         <FaDiscord size={100} />
                     </ActionIcon>
                 </a>
@@ -33,7 +63,7 @@ const NavbarSocials = () => {
                     target="_blank"
                     rel="external noreferrer"
                 >
-                    <ActionIcon size="md" variant="transparent" className={classes.socials}>
+                    <ActionIcon size="md" variant="transparent" className={classes.instagram}>
                         <AiFillInstagram size={100} />
                     </ActionIcon>
                 </a>
@@ -42,7 +72,7 @@ const NavbarSocials = () => {
                     target="_blank"
                     rel="external noreferrer"
                 >
-                    <ActionIcon size="md" variant="transparent" className={classes.socials}>
+                    <ActionIcon size="md" variant="transparent" className={classes.linkedin}>
                         <AiFillLinkedin size={100} />
                     </ActionIcon>
                 </a>
@@ -51,7 +81,7 @@ const NavbarSocials = () => {
                     target="_blank"
                     rel="external noreferrer"
                 >
-                    <ActionIcon size="md" variant="transparent" className={classes.socials}>
+                    <ActionIcon size="md" variant="transparent" className={classes.youtube}>
                         <FaYoutube size={100} />
                     </ActionIcon>
                 </a>
@@ -60,7 +90,7 @@ const NavbarSocials = () => {
                     target="_blank"
                     rel="next noreferrer"
                 >
-                    <ActionIcon size="md" variant="transparent" className={classes.socials}>
+                    <ActionIcon size="md" variant="transparent" className={classes.twitch}>
                         <FaTwitch size={20} />
                     </ActionIcon>
                 </a>
