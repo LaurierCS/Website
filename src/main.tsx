456d7f1b7cbde8911b451932c5834aa9bb1c4002
jc <46619361+juancwu@users.noreply.github.com>
2023-10-22 21:00:34 -0400

    rename js, jsx files to ts, tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './main.css';
 import Router from '@utils/Router';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router/>
-);
+import './main.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Router />);
