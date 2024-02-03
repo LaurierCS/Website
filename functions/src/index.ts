67092a81a5e7bb0847a5cd440d3a94378ae9f02d
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-02-02 21:12:01 -0500

    Add functions to update Firebase events assign unique event UUIDs

---
+++ functions/src/index.ts
@@ -0,0 +1,120 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { Client as NotionClient } from "@notionhq/client";
+import { v4 as uuidv4 } from 'uuid';
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const notionSecret = process.env.NOTION_SECRET
+const databaseId = process.env.NOTION_DATABASE_ID
+
+
+if (!notionSecret || !databaseId) {
+  throw new Error("Environment variables for Notion are not set.");
+}
+
+const notionClient = new NotionClient({
+  auth: notionSecret,
+});
+
+export const setEventIDs = functions.https.onRequest(async (request, response) => {
+  try {
+    const entries = await notionClient.databases.query({
+      database_id: databaseId,
+    });
+
+    const updates = [];
+    for (const page of entries.results) {
+      if ('properties' in page) { 
+        const property = page.properties['Event Identifier']; 
+        if (property.type === "rich_text") { 
+          if (!property.rich_text || property.rich_text.length === 0) {
+
+            const newEventIdentifier = uuidv4();
+            updates.push(notionClient.pages.update({
+              page_id: page.id,
+              properties: {
+                'Event Identifier': {
+                  rich_text: [{
+                    type: 'text',
+                    text: { content: newEventIdentifier },
+                  }],
+                },
+              },
+            }));
+          }
+        }
+      }
+    }
+
+    await Promise.all(updates);
+
+    response.json({ message: 'Database updated successfully', updatedCount: updates.length });
+  } catch (error) {
+    console.error("Error updating Notion calendar:", error);
+    response.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+export const syncNotionEventsToFirestore = functions.https.onRequest(async (_, response) => {
+    try {
+        if (!databaseId) {
+            throw new Error("Environment variable NOTION_DATABASE_ID is not set.");
+        }
+
+        const notionResponse = await notionClient.databases.query({ database_id: databaseId });
+
+		notionResponse.results.forEach(async (page: any) => {
+		    const blockResponse = await notionClient.blocks.children.list({
+		        block_id: page.id,
+		    });
+
+		    const date = page.properties["Date"]?.date?.start ?? "";
+		    const eventDate = date ? new Date(date) : null; 
+
+		    if (!eventDate || eventDate < new Date()) {
+		        return;
+		    }
+
+		    let description = "";
+
+			for (const block of blockResponse.results) {
+
+			    if ('type' in block && block.type === "paragraph" && block.paragraph.rich_text.length > 0) {
+
+			        const firstNonEmptyParagraph = block.paragraph.rich_text.find(rt => rt.plain_text.trim() !== "");
+			        if (firstNonEmptyParagraph) {
+			            description = firstNonEmptyParagraph.plain_text;
+			            break; 
+			        }
+			    }
+			}
+
+		    const title = page.properties["Name"]?.title[0]?.plain_text ?? "Untitled Event";
+		    const eventIdentifier = page.properties["Event Identifier"]?.rich_text[0]?.plain_text ?? ""
+
+			const docData = {
+			    title,
+			    date: eventDate,
+			    description,
+			    place: page.properties["Confirmed Room"]?.multi_select.map((select: any) => select.name).join(", ") ?? "",
+			    icon: page.icon.emoji ?? "✏️",
+			    igPost: "", // There's nothing on the notion, done manually? :(
+    			isPublicDate: page.properties["Date Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
+    			isPublicPlace: page.properties["Place Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
+    			isPublicTime: page.properties["Time Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
+    			visible: page.properties["Visible"]?.multi_select.map((select: any) => select.name).join(", ") ?? true,
+			};
+
+		    await db.collection("events").doc(eventIdentifier).set(docData, { merge: true });
+		});
+
+        response.send("Notion events synced successfully with Firestore.");
+    } catch (error) {
+        console.error("Error syncing Notion events to Firestore:", error);
+        response.status(500).send("Internal Server Error");
+    }
+});
