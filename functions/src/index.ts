7123a7d445749b0aa593615ef0168803791fa3dc
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-02-08 21:27:28 -0500

    Fix spacings in code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,62 +20,62 @@ const notionClient = new NotionClient({
 
 //export const syncNotionEventsToFirestore = functions.https.onRequest(async (_, response) => {
 export const scheduledSyncNotionEventsToFirestore = functions.pubsub.schedule('0 * * * *').onRun(async (context) => {
-    try {
-        if (!databaseId) {
-            throw new Error("Environment variable NOTION_DATABASE_ID is not set.");
-        }
+	try {
+		if (!databaseId) {
+			throw new Error("Environment variable NOTION_DATABASE_ID is not set.");
+		}
 
-        const notionResponse = await notionClient.databases.query({ database_id: databaseId });
+		const notionResponse = await notionClient.databases.query({ database_id: databaseId });
 
 		notionResponse.results.forEach(async (page: any) => {
-		    const blockResponse = await notionClient.blocks.children.list({
-		        block_id: page.id,
-		    });
+			const blockResponse = await notionClient.blocks.children.list({
+				block_id: page.id,
+			});
 
-		    const date = page.properties["Date"]?.date?.start ?? "";
-		    const eventDate = date ? new Date(date) : null; 
+			const date = page.properties["Date"]?.date?.start ?? "";
+			const eventDate = date ? new Date(date) : null; 
 
-		    if (!eventDate || eventDate < new Date()) {
-		        return;
-		    }
+			if (!eventDate || eventDate < new Date()) {
+				return;
+			}
 
-		    let description = "";
+			let description = "";
 
 			for (const block of blockResponse.results) {
 
-			    if ('type' in block && block.type === "paragraph" && block.paragraph.rich_text.length > 0) {
+				if ('type' in block && block.type === "paragraph" && block.paragraph.rich_text.length > 0) {
 
-			        const firstNonEmptyParagraph = block.paragraph.rich_text.find(rt => rt.plain_text.trim() !== "");
-			        if (firstNonEmptyParagraph) {
-			            description = firstNonEmptyParagraph.plain_text;
-			            break; 
-			        }
-			    }
+					const firstNonEmptyParagraph = block.paragraph.rich_text.find(rt => rt.plain_text.trim() !== "");
+					if (firstNonEmptyParagraph) {
+						description = firstNonEmptyParagraph.plain_text;
+						break; 
+					}
+				}
 			}
 
-		    const title = page.properties["Name"]?.title[0]?.plain_text ?? "Untitled Event";
+			const title = page.properties["Name"]?.title[0]?.plain_text ?? "Untitled Event";
 
 			const docData = {
-			    title,
-			    date: eventDate,
-			    description,
-			    place: page.properties["Confirmed Room"]?.multi_select.map((select: any) => select.name).join(", ") ?? "",
-			    icon: page.icon.emoji ?? "✏️",
-			    igPost: "", // There's nothing on the notion, done manually? :(
-    			isPublicDate: page.properties["Date Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
-    			isPublicPlace: page.properties["Place Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
-    			isPublicTime: page.properties["Time Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
-    			visible: page.properties["Visible"]?.multi_select.map((select: any) => select.name).join(", ") ?? true,
+				title,
+				date: eventDate,
+				description,
+				place: page.properties["Confirmed Room"]?.multi_select.map((select: any) => select.name).join(", ") ?? "",
+				icon: page.icon.emoji ?? "✏️",
+				igPost: "", // There's nothing on the notion, done manually? :(
+				isPublicDate: page.properties["Date Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
+				isPublicPlace: page.properties["Place Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
+				isPublicTime: page.properties["Time Public"]?.multi_select.map((select: any) => select.name).join(", ") ?? false,
+				visible: page.properties["Visible"]?.multi_select.map((select: any) => select.name).join(", ") ?? true,
 			};
 
-		    const docId = page.id.replace(/-/g, "");
+			const docId = page.id.replace(/-/g, "");
 
-		    await db.collection("events").doc(docId).set(docData, { merge: true });
+			await db.collection("events").doc(docId).set(docData, { merge: true });
 		});
 		console.log("Notion events synced successfully with Firestore.");
-        //response.send("Notion events synced successfully with Firestore.");
-    } catch (error) {
-        console.error("Error syncing Notion events to Firestore:", error);
-        //response.status(500).send("Internal Server Error");
-    }
+		//response.send("Notion events synced successfully with Firestore.");
+	} catch (error) {
+		console.error("Error syncing Notion events to Firestore:", error);
+		//response.status(500).send("Internal Server Error");
+	}
 });
