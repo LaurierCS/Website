99e1229ebbcb3f2d591f2730484d1cdaccea725f
jc <46619361+juancwu@users.noreply.github.com>
2024-03-06 14:20:37 -0500

    export firebase/storage instance

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import store from "./firestore";
 import auth from "./auth";
+import storage from "./storage";
 
-export { app, store, auth };
+export { app, store, auth, storage };
