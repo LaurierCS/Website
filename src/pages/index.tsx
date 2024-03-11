7e2daaee5819d4da7f029eddeac6ede99bce410f
Jacob Jung1~ <jacobj@live.ca>
2024-03-11 18:45:41 -0400

    Add Error404

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Landing from "./Landing/Landing";
 import Login from "./Admin/Login/Login";
 import AdminPage from "./Admin/AdminPage";
-
-export { Landing, Login, AdminPage };
+import Error404 from "./Error404/Error404";
+export { Landing, Login, AdminPage, Error404 };
