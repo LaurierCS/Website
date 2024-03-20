d910d8fbfc6a11ef2df608d39c8da6eb716aa5eb
Jacob Jung1~ <jacobj@live.ca>
2024-03-20 17:07:37 -0400

    Fixed eslint

--- src/pages/Error404/Error404.tsx
+++ src/pages/Error404/Error404.tsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
-import { Button } from '@mantine/core'; 
-import './Error404.css';
+import { Link } from "react-router-dom";
+import { Button } from "@mantine/core"; 
+import "./Error404.css";
 
 const Error404 = () => {
-  return (
-    <div className="error-404-container">
-      <h1>404 Not Found</h1>
-      <p>This is not the page you are looking for</p>
-      <Link to="/" style={{ textDecoration: 'none' }}>
-        <Button
-          variant="gradient"
-          gradient={{ from: 'indigo', to: 'cyan' }}
-          style={{ marginTop: '20px', marginBottom: '20px', marginLeft: 'auto', marginRight: 'auto', display: 'block' }}
-        >
+    return (
+        <div className="error-404-container">
+            <h1>404 Not Found</h1>
+            <p>This is not the page you are looking for</p>
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <Button
+                    variant="gradient"
+                    gradient={{ from: "indigo", to: "cyan" }}
+                    style={{ marginTop: "20px", marginBottom: "20px", marginLeft: "auto", marginRight: "auto", display: "block" }}
+                >
           Come Home ❤️
-        </Button>
-      </Link>
-    </div>
-  );
+                </Button>
+            </Link>
+        </div>
+    );
 };
 
 export default Error404;
